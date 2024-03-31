184c7284b32a983388e5c2ba3935b7f76bc80363
Sholawasiu <sholawasiu92@gmail.com>
2024-03-31 09:55:58 -0700

    simpleshell

---
+++ print_command.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/*
+ * print command
+ *
+ * description: print command to stdout
+ *
+ */
+
+ int print_command()
+ {
+ 	int *command = "cisfun$";
+	int write(2, command, 1);
+
+
+
+ }
